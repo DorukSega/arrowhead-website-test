56dd54344a950b8b3e00510f02aee674d3bc9222
Doruk Yılmaz <dorukyogscast@gmail.com>
2021-05-30 00:56:05 +0300

    Commit Version 0.1.8 Some code adjustments Rss code is written Now it needs to be visualized

--- js/base.js
+++ js/base.js
@@ -3,26 +3,50 @@ function load() {
     if (navigator.userAgent.indexOf('Trident/') > 0 || navigator.userAgent.indexOf('MSIE ') > 0) {
         document.body.innerHTML = "<svg width='512px' height='128px' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 2500 306'><path data-name='ARROWHEAD copy'  d='M296.789,294.768a9.19,9.19,0,0,0-.4-1.619L197.647,11.42c-0.81-2.429-2.428-3.643-4.452-3.643H108.216a4.466,4.466,0,0,0-4.047,3.238L0.575,293.149a9.179,9.179,0,0,0-.4,1.619c0,2.024,1.619,3.238,4.047,3.238H68.559a4.018,4.018,0,0,0,3.237-2.429l18.21-52.621h99.143l17,52.621a3.667,3.667,0,0,0,3.642,2.429h83.361C295.575,298.006,296.789,296.792,296.789,294.768ZM172.557,187.5H108.216l32.778-98.766ZM547.445,294.768a2.974,2.974,0,0,0-.81-2.429L458.823,169.69c40.062-10.119,63.128-41.288,63.128-78.932,0-44.121-35.206-82.98-91.859-82.98H293.72a3.627,3.627,0,0,0-3.642,3.643V294.363a3.627,3.627,0,0,0,3.642,3.643h72.839c2.024,0,3.238-1.619,3.238-3.643V193.977h20.638l65.96,102.41a10,10,0,0,0,3.642,1.619H543.4C545.826,298.006,547.445,296.387,547.445,294.768ZM436.162,103.711c0,23.477-16.187,39.668-36.42,39.668H369.8V65.256h29.54C422,65.256,436.162,81.852,436.162,103.711ZM793.491,294.768a2.974,2.974,0,0,0-.81-2.429L704.869,169.69C744.931,159.571,768,128.4,768,90.758c0-44.121-35.206-82.98-91.859-82.98H539.766a3.627,3.627,0,0,0-3.642,3.643V294.363a3.627,3.627,0,0,0,3.642,3.643h72.839c2.024,0,3.238-1.619,3.238-3.643V193.977h20.638l65.96,102.41a10,10,0,0,0,3.642,1.619h83.361C791.872,298.006,793.491,296.387,793.491,294.768ZM682.208,103.711c0,23.477-16.187,39.668-36.42,39.668H615.843V65.256h29.54C668.045,65.256,682.208,81.852,682.208,103.711ZM1069.9,151.475C1069.9,61.208,1010.01,1.3,919.768,1.3c-95.5,0-149.726,71.242-149.726,150.175,0,93.1,60.7,152.6,149.726,152.6C1005.56,304.078,1069.9,239.313,1069.9,151.475Zm-83.362-.405c0,56.265-30.755,84.6-67.175,84.6s-65.96-33.193-65.96-84.6c0-49.788,27.113-82.171,65.96-82.171C955.378,68.9,986.538,91.162,986.538,151.07ZM1458.32,11.015c0-2.024-.81-3.238-3.24-3.238h-64.34a5.854,5.854,0,0,0-4.05,2.833l-44.11,181.748L1294.83,10.611a5.125,5.125,0,0,0-3.64-2.833H1231.3c-1.21,0-2.83,1.619-3.24,2.833l-48.15,181.748L1129.33,10.611a5.16,5.16,0,0,0-3.65-2.833H1048.8c-2.43,0-3.24,1.214-3.24,3.238v1.619l84.17,282.943a4.645,4.645,0,0,0,3.64,2.429H1205a4.007,4.007,0,0,0,3.23-2.429l43.71-153.412,43.3,153.412c0.4,1.215,2.83,2.429,4.04,2.429h71.63a4.645,4.645,0,0,0,3.64-2.429l83.36-282.943C1457.91,12.23,1458.32,11.42,1458.32,11.015Zm257.02,283.348V11.42c0-2.024-1.22-3.643-3.24-3.643h-77.7a3.628,3.628,0,0,0-3.64,3.643V123.381h-99.14V11.42a3.628,3.628,0,0,0-3.64-3.643h-76.49a3.628,3.628,0,0,0-3.64,3.643V294.363a3.628,3.628,0,0,0,3.64,3.643h76.49a3.628,3.628,0,0,0,3.64-3.643v-112.2h99.14v112.2a3.628,3.628,0,0,0,3.64,3.643h77.7C1714.12,298.006,1715.34,296.387,1715.34,294.363Zm211.14,0,20.38-53.836a3.628,3.628,0,0,0-3.64-3.643H1817.28v-55.05h74.46c2.02,0,3.24-1.62,3.24-3.644v-50.6c0-2.024-1.22-4.048-3.24-4.048h-74.46V68.9h129.09a3.628,3.628,0,0,0,3.64-3.643V11.42a3.628,3.628,0,0,0-3.64-3.643H1737.16a3.63,3.63,0,0,0-3.65,3.643V294.363a3.63,3.63,0,0,0,3.65,3.643h185.68A3.628,3.628,0,0,0,1926.48,294.363Zm314.85,0.4a9.264,9.264,0,0,0-.4-1.619L2142.19,11.42c-0.81-2.429-2.43-3.643-4.45-3.643h-84.98a4.474,4.474,0,0,0-4.05,3.238L1945.12,293.149a9.08,9.08,0,0,0-.41,1.619c0,2.024,1.62,3.238,4.05,3.238h64.34a4.007,4.007,0,0,0,3.24-2.429l18.21-52.621h99.14l17,52.621a3.665,3.665,0,0,0,3.64,2.429h83.36C2240.12,298.006,2241.33,296.792,2241.33,294.768ZM2117.1,187.5h-64.34l32.78-98.766Zm382.17-34.811c0-69.622-38.85-144.912-154.17-144.912H2238.26a3.628,3.628,0,0,0-3.64,3.643V294.363a3.628,3.628,0,0,0,3.64,3.643h113.31C2450.31,298.006,2499.27,226.36,2499.27,152.689Zm-87,0c0,61.527-34.4,84.2-67.17,84.2h-30.76V68.9h29.14C2376.25,68.9,2412.27,84.686,2412.27,152.689Z'/></svg><h2>About</h2><h3>A classified Half-Life 2 mod about familiar places and unfamiliar dangers.</h3><h2>Team</h2><h3>• The Tax Collector Man</h3><h3>• Gmad</h3><h3>• Emily</h3><h3>• Dana Cief</h3><h2>Links</h2><h3><a target='_blank' rel='noreferrer' href='https://twitter.com/ArrowheadDev'>Twitter</a></h3><h1>Internet Explorer 🎉</h1>";
     }
+    //I guess this is a hidden feature now because
+    //changing the anchor of the site is very performance intensive
+    if (window.location.hash == "#news") {
+        scrollto('.newscont');
+    } else if (window.location.hash == "#faq") {
+        scrollto('.faqcont');
+    } else if (window.location.hash == "#team") {
+        scrollto('.teamcont');
+    } else if (window.location.hash == "#about") {
+        scrollto('.aboutcont');
+    }
+    //loading the updates
+
 }
 
-function postfeed() {
-    /* guidlines for later:
-    make a array
+function scrollto(item) {
+    document.querySelector(item).scrollIntoView();
+}
+
+function loadfeed() {
+    /* guidlines:
     0: title
-    1: first paragraph or preview but cut it in js to 2 lines or so "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor..."
-    2: first image 
-    3: link
-    Then put it in news section
+    1: link
+    2: date
+    3: creator
+    4: first paragraph
+    5: first image 
     */
     var posts = [];
     let parser = new RSSParser();
     parser.parseURL('http://' + window.location.hostname + ':4050/medium', function(err, feed) {
         if (err) throw err;
-        console.log(feed.title);
-        feed.items.forEach(function(entry) {
-            console.log(entry.title + ':' + entry.link);
+        feed.items.forEach(function(entry, index) {
+            posts.push([]);
+            posts[index].push(entry.title);
+            posts[index].push(entry.link);
+            posts[index].push(entry.pubDate);
+            posts[index].push(entry.creator);
+            posts[index].push(entry["content:encoded"].split("<p>")[1].split("</p>")[0].slice(0, 56) + "...");
+            posts[index].push(entry["content:encoded"].split("src=\"")[1].split("\"")[0]);
         })
+        console.log(posts);
     })
+    return posts;
 }
 
 window.onscroll = function() {
@@ -36,27 +60,30 @@ window.onscroll = function() {
 
     }
     //
-    if (document.documentElement.scrollTop > document.querySelector('.newscont').offsetTop & document.documentElement.scrollTop < document.querySelector('.faqcont').offsetTop) {
+    if (document.documentElement.scrollTop >= document.querySelector('.newscont').offsetTop & document.documentElement.scrollTop < document.querySelector('.faqcont').offsetTop) {
         document.querySelectorAll(".option").forEach(function(val) {
             val.classList.remove("selected");
         });
         document.querySelectorAll(".newsoption").forEach(function(val) {
             val.classList.add("selected");
         });
-    } else if (document.documentElement.scrollTop > document.querySelector('.faqcont').offsetTop & document.documentElement.scrollTop < document.querySelector('.teamcont').offsetTop) {
+        //changeanchor("news");
+    } else if (document.documentElement.scrollTop >= document.querySelector('.faqcont').offsetTop & (document.documentElement.scrollTop < document.querySelector('.teamcont').offsetTop & (document.documentElement.scrollTop + window.innerHeight < document.documentElement.offsetHeight))) {
         document.querySelectorAll(".option").forEach(function(val) {
             val.classList.remove("selected");
         });
         document.querySelectorAll(".faqoption").forEach(function(val) {
             val.classList.add("selected");
         });
-    } else if (document.documentElement.scrollTop > document.querySelector('.teamcont').offsetTop) {
+        //changeanchor("faq");
+    } else if ((document.documentElement.scrollTop >= document.querySelector('.teamcont').offsetTop) || (document.documentElement.scrollTop + window.innerHeight >= document.documentElement.offsetHeight)) {
         document.querySelectorAll(".option").forEach(function(val) {
             val.classList.remove("selected");
         });
         document.querySelectorAll(".teamoption").forEach(function(val) {
             val.classList.add("selected");
         });
+        //changeanchor("team");
     } else if (document.querySelector('.aboutoption').classList.contains("selected") == false) {
         document.querySelectorAll(".option").forEach(function(val) {
             val.classList.remove("selected");
@@ -64,6 +91,7 @@ window.onscroll = function() {
         document.querySelectorAll(".aboutoption").forEach(function(val) {
             val.classList.add("selected");
         });
+        //changeanchor("");
     }
 };
 
